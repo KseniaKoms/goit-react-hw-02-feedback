f49251b1da48daf91dc9a09286b99fe0d1e19ed5
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-07-06 11:21:04 +0300

    partly done first task

---
+++ src/components/FeedbackButtons/FeedbackButtons.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FeedbackButtons = () => {
+    return (
+        <div>
+            <button type="button">Good</button>
+            <button type="button">Neutral</button>
+            <button type="button">Bad</button>
+        </div>
+    )
+}
+
+export default FeedbackButtons
