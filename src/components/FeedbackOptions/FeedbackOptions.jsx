e5cfd59348d4b7addc4f7ca0fd7a62a9fab51aef
Ksenia Komisarova <cardavar.ksen@gmail.com>
2022-07-10 01:17:10 +0300

    addsome style

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,21 +1,26 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
+import { OptionsBtn, OptionsList } from './FeedbackOptions.styled';
 
-const FeedbackOptions = ({ options, handleOptionClick}) => {
-    return (
-        <ul>
-            {options.map(option => {
-                return (<li key={option}>
-                    <button type="button" onClick={() => handleOptionClick(option)}>{ option}</button>
-                </li>);
-            })}
-        </ul>
-    )
-}
+const FeedbackOptions = ({ options, handleOptionClick }) => {
+  return (
+    <OptionsList>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <OptionsBtn type="button" onClick={() => handleOptionClick(option)}>
+              {option}
+            </OptionsBtn>
+          </li>
+        );
+      })}
+    </OptionsList>
+  );
+};
 
 export default FeedbackOptions;
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    handleOptionClick: PropTypes.func.isRequired,
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleOptionClick: PropTypes.func.isRequired,
+};
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const OptionsList = styled.ul`
+  display: flex;
+`;
+
+export const OptionsBtn = styled.button`
+  background: #3cabef;
+  border: none;
+  color: #fff;
+  padding: 10px 30px;
+  border-radius: 999px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+  justify-content: center;
+
+  :hover {
+    background-color: #007bc7;
+  }
+`;
